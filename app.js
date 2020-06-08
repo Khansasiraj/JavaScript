6a3cd83790ae7cfaa1f32ba8234e2817f538e1a9
Khansa <57318947+Khansasiraj@users.noreply.github.com>
2020-06-09 01:21:31 +0500

    16 chapters done

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-// ----------------------------------------------------------Chapter_01----------------------------------------------------------
+// -----------------------------------------------------  Chapter_01 (Alerts) -----------------------------------------------------
+
+//1. Write a script to greet your website visitor using JS alert box. 
 alert("Welcome To My Website");
+
+//2. Write a script to display following message on your web page: 
 alert("Error! Please Enter a valid password");
+
+//3. Write a script to display following message on your web page: (Hint : Use line break) 
 alert("Welcome to JS Land.. \nHappy Coding!");
+
+//4. Write a script to display following messages in sequence: 
 alert("Welcome to JS Land..");
 alert("Happy Coding!");
+
+//5. Generate the following message through browser’s developer console: 
 console.log("Hello.. I can run JS through my web browser console");
 
+// -----------------------------------------------------  Chapter_01 End -----------------------------------------------------
 
-// ----------------------------------------------------------Chapter_02----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------  Chapter_02 (VARIABLES FOR STRINGS ) -----------------------------------------------------
+
+//1. Declare a variable called username. 
 var username;
+
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
 var Myname = "Khansa Siraj";
 
+
+// 3. Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
 var message;
 message = "Hello World";
 alert(message);
 
-//Student Bio Data
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. 
 var name = "Jhone Doe";
 var age = "15 years old";
 var course = "Certified Mobile Application Development";
@@ -23,41 +54,94 @@ alert(name);
 alert(age);
 alert(course);
 
+
+//5. Write a script to display the following alert using one JS variable: 
 var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
 alert(pizza);
 
+
+//6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation) 
 var email = "Khansasiraj@yahoo.com";
 alert("My email address is" + " " + email);
 
+
+//7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. 
+//Display the following message in an alert box: 
 var book = "A smarter way to learn JavaScript";
 alert("I am trying to learn from the Book" + " " + book);
 
+
+//8. Write a script to display this in browser through JS 
 document.write("Yeah! I can write HTML content through JavaScript");
 
+
+//9. Store following string in a variable and show in alert and browser through JS 
+//“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” 
 var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 alert(symbol);
+// -----------------------------------------------------  Chapter_02 End -----------------------------------------------------
 
-// ----------------------------------------------------------Chapter_03----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------  Chapter_03 (VARIABLES FOR NUMBERS ) -----------------------------------------------------
+
+//1. Declare a variable called age & assign to it your age. Show your age in an alert box. 
 var age = 23;
 alert("I am" + " " + age + " " + "years old");
 
+
+//2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+//Show his/her number of visits on your web page. For example: “You have visited this site N times”. 
 var n = 12;
 alert("You have visited this site " + n + " times");
 
+
+//3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser: 
 var birthYear = 1998;
 document.write("My birth year is " + birthYear);
 document.write("<br>")
-document.write("Data type of my declared variable is" + " " + typeof birthYear);
+document.write("\n" + "Data type of my declared variable is" + " " + typeof birthYear);
 
+
+document.write("<br>" + "<br>");
+
+
+//4. A visitor visits an online clothing store www.xyzClothing.com . 
+// Write a script to store in variables the following information: 
+// a. Visitor’s name 
+// b. Product title 
+// c. Quantity 
+// i.e. how many products a visitor wants to order 
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. 
 var Visitor_name = "John Doe";
 var Product_title = "T-shirt(s)";
 var Quantity = 5;
 document.write(Visitor_name + " " + "ordered" + " " + Quantity + " " + Product_title + " " + "on XYZ Clothing store");
+// -----------------------------------------------------  Chapter_03 End -----------------------------------------------------
 
 
-// ----------------------------------------------------------Chapter_04----------------------------------------------------------
-// 3 variables in one statement.
+
+
+
+
+
+
+
+
+// -------------------------------------------  Chapter_04 (VARIABLE NAMES: LEGAL & ILLEGAL ) -----------------------------------------------------
+
+//1. Declare 3 variables in one statement. 
 var name, email, qualification;
+
+//2. Declare 5 legal & 5 illegal variable names. 
 
 //5 legal variable names.
 var chat;
@@ -74,20 +158,46 @@ var $name;
 // var if;
 // var 1stcustomer;
 
-// document.write("<h1>Rules for naming JS variables</h1>");
-document.write("<h1>" + "Rules for naming JS variables" + "</h1>");
+
+// 3. Display this in your browser 
+
+// a)  A heading stating “Rules for naming JS variables” 
+document.write("<h1>Rules for naming JS variables</h1>");
+
+// b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable 
 document.write("Variable names can only contain numbers, $ and _. For example: $my_1stVariable." + "<br/>");
+
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name 
 document.write("Variable must begin with a letter, $ or _. For example: $name, _name or name" + "<br/>");
+
+// d) Variable names are case _________ 
 document.write(" Variable names are case sensitive" + "<br/>");
+
+// e) Variable names should not be JS _________   
 document.write("Variable names should not be JS keywords" + "<br/>");
+// -----------------------------------------------------  Chapter_04 End -----------------------------------------------------
 
 
-// ----------------------------------------------------------Chapter_05----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------  Chapter_05 (MATH EXPRESSIONS ) -----------------------------------------------------
+
+//1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.  
 var a = 3;
 var b = 5;
 // addition
 var c = a + b;
 document.write("Sum of" + " " + a + " " + "and" + " " + b + " " + "is" + " " + c);
+
+
+//2. Repeat task1 for subtraction, multiplication, division & modulus. 
 
 // subtraction
 var c = a - b;
@@ -109,35 +219,54 @@ document.write("<br>" + "Mod of" + " " + a + " " + "and" + " " + b + " " + "is" 
 document.write("<br>" + "<br>");
 
 
-//Do the following using JS Mathematic Expressions 
+// 3. Do the following using JS Mathematic Expressions 
+// a. Declare a variable.      
 var x;
+
+//b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
 document.write("Value after variable declaration is:" + " " + x);
 
+// c. Initialize the variable with some number.
 x = 5;
 document.write("<br>");
+
+//d. Show the value of variable in your browser like “Initial value: 5”.
 document.write("Initial value: " + " " + x);
 
+//e. Increment the variable.
 x = ++x;
 document.write("<br>");
+
+//f. Show the value of variable in your browser like “Value after increment is: 6”.
 document.write("Value after increment is:  " + " " + x);
 
+//g. Add 7 to the variable.
 x = x + 7;
 document.write("<br>");
+
+// h. Show the value of variable in your browser like “Value after addition is: 13”.  
 document.write("Value after addition is:   " + " " + x);
 
+// i. Decrement the variable.
 x = --x;
 document.write("<br>");
+
+//j. Show the value of variable in your browser like “Value after decrement is: 12”.
 document.write("Value after decrement is: " + " " + x);
 
+// k. Show the remainder after dividing the variable’s value by 3.
 x = x % 3;
 document.write("<br>");
+
+// l. Output : “The remainder is : 0”.
 document.write("The remainder is :    " + " " + x);
 
 
 document.write("<br>" + "<br>");
 
 
-// script to store ticket price
+// 4. Cost of one movie ticket is 600 PKR.
+// Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output: 
 var ticket = 600;
 var quantity = 5;
 var cost = ticket * quantity;
@@ -148,7 +277,7 @@ document.write("Total cost to buy" + " " + quantity + " " + "tickets to movie is
 document.write("<br>" + "<br>");
 
 
-//code of table
+//5. Write a script to display multiplication table of any number in your browser.
 var no = 4;
 var x = 0;
 document.write(no + " x " + ++x + " = " + (no * x) + "<br/>");
@@ -165,7 +294,11 @@ document.write(no + " x " + ++x + " = " + (no * x));
 
 document.write("<br>" + "<br>");
 
-//  The Temperature Converter
+// 6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here. 
+// a.  Store a Celsius temperature into a variable. 
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”. 
+// c. Now store a Fahrenheit temperature into a variable. 
+// d. Convert it to Celsius & output “NNoF is NNoC”. 
 
 //celcius to farenheight
 var C = 25;
@@ -181,21 +314,28 @@ document.write(F + "F is " + C + "C" + "<br/>");
 document.write("<br>" + "<br>");
 
 
-// program to implement checkout process of a shopping cart system for an e-commerce website
+// 7.  Write a program to implement checkout process of a shopping cart system for an e-commerce website. 
+// Store the following in variables 
+
+// a. Price of item 1 
+// b. Price of item 2 
 var item1_Price = 650;
 var item2_Price = 100;
 
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2 
 var item_1_quantity = 3;
 var item_2_quantity = 7;
 
 var item_1_cost = item1_Price * item_1_quantity;
 var item_2_cost = item2_Price * item_2_quantity;
 
+//  e. Shipping charges 
 var Shipping = 100;
 
 var total = item_1_cost + item_2_cost + Shipping;
 
-//Printing Reciept
+// Compute the total cost & show the receipt in your browser. 
 document.write("Price of Item 1 is " + item1_Price + "<br/>");
 document.write("Quantity of Item 1 is " + item_1_quantity + "<br/>");
 document.write("Price of Item 2 is " + item2_Price + "<br/>");
@@ -207,7 +347,8 @@ document.write("Total cost of your order is " + total);
 document.write("<br>" + "<br>");
 
 
-//marksheet
+//8. Store total marks & marks obtained by a student in 2 variables. 
+// Compute the percentage & show the result in your browser 
 var totalMarks = 980;
 var obttainMarks = 804;
 var Percentage = (totalMarks / obttainMarks) * 100;
@@ -221,7 +362,9 @@ document.write("Percentage: " + Percentage);
 document.write("<br>" + "<br>");
 
 
-//Currency conversion
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. 
+// Perform all calculations in a single expression. 
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee) 
 var dollarRate = 104.80;
 var riyalRate = 28;
 var pkr = (10 * dollarRate) + (25 * riyalRate);
@@ -232,7 +375,11 @@ document.write("Total Currency in PKR: " + pkr);
 document.write("<br>" + "<br>");
 
 
-//Arithmetic calculations
+//10. Write a program to initialize a variable with some number and do arithmetic in following sequence: 
+// a. Add 5 
+// b. Multiply by 10 
+// c. Divide the result by 2 
+// Perform all calculations in a single expression 
 var num = 7;
 num = (((num + 5) * 10) / 2);
 document.write("<h3>" + "Arithmetic Operation in single statement" + "</h3>");
@@ -242,7 +389,10 @@ document.write("Output: " + num);
 document.write("<br>" + "<br>");
 
 
-//Age Calculator
+//11. The Age Calculator: Forgot how old someone is? Calculate it! 
+// a. Store the current year in a variable. 
+// b. Store their birth year in a variable. 
+// c. Calculate their 2 possible ages based on the stored values. 
 var currentYear = 2020;
 var birthYear = 1998;
 var Age = currentYear - birthYear;
@@ -256,7 +406,10 @@ document.write("Your age is: " + Age);
 document.write("<br>" + "<br>");
 
 
-// Calculate properties of a circle. a. Store a radius into a variable. 
+// 12. The Geometrizer: Calculate properties of a circle. 
+// a. Store a radius into a variable. 
+// b. Calculate the circumference based on the radius, and output “The circumference is NN”. (Hint : Circumference of a circle = 2 π r , π = 3.142) 
+// Calculate the area based on the radius, and output “The  area is NN”. (Hint : Area of a circle = π r2, π = 3.142) 
 var radius = 7;
 var circumference = 2 * 3.142 * radius;
 var area = 3.142 * radius * radius;
@@ -270,7 +423,12 @@ document.write("The area is: " + area);
 document.write("<br>" + "<br>");
 
 
-// The Lifetime Supply Calculator
+// 13. The Lifetime Supply Calculator: Ever wonder how much a “lifetime supply” of your favorite snack is? Wonder no more. 
+// a. Store your favorite snack into a variable 
+// b. Store your current age into a variable. 
+// c. Store a maximum age into a variable. 
+// d. Store an estimated amount per day (as a number). 
+// e. Calculate how many would you eat total for the rest of your life. 
 var snack = "Chocalate Chip";
 var age = 22;
 var maxAge = 60;
@@ -285,6 +443,14 @@ document.write("Current Age: " + age + "<br/>");
 document.write("Estimated Maximum Age: " + maxAge + "<br/>");
 document.write("Amount of snacks per day: " + amountPerDay + "<br/>");
 document.write("You will need " + eat + " to last you until the ripe old age of " + maxAge);
+// -----------------------------------------------------  Chapter_05 End -----------------------------------------------------
+
+
+
+
+
+
+
 
 
 
@@ -448,6 +614,8 @@ document.write("</tr>");
 
 document.write("</Table>");
 // ----------------------------------------  Chapter_06 to 09 End -----------------------------------------
+
+
 
 
 
@@ -737,6 +905,8 @@ document.write("<br>" + "<br>");
 
 
 
+
+
 // ---------------------------------  Chapter_12 to 13 (IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS ) -----------------------------------
 
 // 1. Write a program that takes a character (number or string) in a variable & 
@@ -882,6 +1052,12 @@ else {
         alert("Invalid Input");
 }
 // ----------------------------------------  Chapter_12 to 13 End -----------------------------------------
+
+
+
+
+
+
 
 
 
@@ -1034,7 +1210,7 @@ document.write("<br>" + "<br>");
 
 //10. Write a program to store student scores in an array & sort the array in ascending order using Array’s sort method. 
 var arr = [59, 25, 66, 45, 75, 23];
-document.write("students scores : " + "<br>" + arr + "<br>");
+document.write("students scores : " + "<br>" + arr + "<br>" + "<br>");
 
 var sorted = arr.sort()
 document.write("sorted scores : " + "<br>" + sorted);
@@ -1045,7 +1221,7 @@ document.write("<br>" + "<br>");
 
 //11. Write a program to initialize an array with city names. Copy 3 array elements from cities array to selectedCities array. 
 var cities = ["karachi", "lahore", "murre", "islamabad", "hyderabad", "quetta"];
-document.write("cities are" + "<br>" + cities + "<br>");
+document.write("cities are" + "<br>" + cities + "<br>" + "<br>");
 
 var selectedCities = cities.slice(1, 4);
 document.write("selected cities are" + "<br>" + selectedCities);
@@ -1058,16 +1234,84 @@ document.write("<br>" + "<br>");
 // var arr = [“This ”, “ is ”, “ my ”, “ cat”]; 
 // (Use array’s join method) 
 var arr = ["This", "is", "my", "cat"];
+document.write("Array" + "<br>" + arr + "<br>" + "<br>");
+
 var arr1 = arr.join(" ")
-document.write(arr1);
+document.write("String" + "<br>" + arr1);
+
+
+document.write("<br>" + "<br>");
+
+
+// 13. Create a new array. 
+// Store values one by one in such a way that you can access the values in the order in which they were stored. 
+// (FIFO-First In First Out) 
+var devices = ["Keyboard", "Mouse", "Printer", "Monitor"];
+var out;
+document.write("Devices:" + "<br/>" + devices + "<br/>" + "<br/>");
+
+out = devices.shift();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.shift();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.shift();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.shift();
+document.write("Out:" + "<br/>" + out);
+
+
+document.write("<br>" + "<br>");
+
+
+// 14. Create a new array. 
+// Store values one by one in such a way that you can access the values in reverse order. 
+// (Last InFirst Out) 
+var devices = ["Keyboard", "Mouse", "Printer", "Monitor"];
+var out;
+document.write("Devices:" + "<br/>" + devices + "<br/>" + "<br/>");
+
+out = devices.pop();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.pop();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.pop();
+document.write("Out:" + "<br/>" + out + "<br/>");
+out = devices.pop();
+document.write("Out:" + "<br/>" + out);
+
+
+document.write("<br>" + "<br>");
+
+
+// 15. Write a program to store phone manufacturers (Apple, Samsung, Motorola, Nokia, Sony & Haier) in an array. 
+// Display the following dropdown/select menu in your browser using document.write() method: 
+var phone = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+var i = 0;
+
+document.write("<select>");
+document.write("<option>" + phone[i++] + "</option>");
+document.write("<option>" + phone[i++] + "</option>");
+document.write("<option>" + phone[i++] + "</option>");
+document.write("<option>" + phone[i++] + "</option>");
+document.write("<option>" + phone[i++] + "</option>");
+document.write("</select>");
 
 
 document.write("<br>" + "<br>");
 // ----------------------------------------  Chapter_14 to 16 End -----------------------------------------
 
-// ---------------------------------  Chapter_06 (MATH EXPRESSIONS) -----------------------------------
 
-// ----------------------------------------  Chapter_06 End -----------------------------------------
+
+
+
+
+
+
+
+
+// ---------------------------------  Chapter_17 to 20 (ARRAYS AND LOOP ) -----------------------------------
+
+// ----------------------------------------  Chapter_17 to 20 End -----------------------------------------
 
 // ---------------------------------  Chapter_06 (MATH EXPRESSIONS) -----------------------------------
 
